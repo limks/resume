9e9cf3c56182d1751e320a5cbcd8ed3b3d92e4c6
limks <35590886+limks@users.noreply.github.com>
2018-05-15 11:14:27 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+/*
+    ###############################################################################
+    #
+    #  Copyright © 2018 by <yourname>
+    #
+    #   All rights reserved. No part of this publication may be reproduced, distributed,
+    #   or transmitted in any form or by any means, including photocopying, recording, 
+    #   or other electronic or mechanical methods, without the prior written permission 
+    #   of the publisher, except in the case of brief quotations embodied in critical reviews 
+    #   and certain other noncommercial uses permitted by copyright law.
+    #
+    #   PERSONAL RESUME - JAVASCRIPT.JS!
+    #
+    ###############################################################################
+*/
